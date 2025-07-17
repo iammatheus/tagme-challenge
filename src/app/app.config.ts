e61cd5fc6938daf4e808a39cc1708ffb67511576
Matheus Ferreira <matheus.ferreira9@hotmail.com>
2025-07-17 12:49:58 -0300

    Modal e cadastro de produtos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    provideAnimations(),
   ],
 };
--- src/app/pages/products/services/product.service.ts
+++ src/app/pages/products/services/product.service.ts
@@ -1,21 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { environment as env } from '../../../../environments/environment.local';
-import { IProduct } from '../../../../core/interfaces/IProduct';
+import { IProduct, IProductItem } from '../../../../core/interfaces/IProduct';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  private readonly API = env.API_URL;
+  private readonly API_URL = env.API_URL;
 
   constructor(private http: HttpClient) {}
 
   get(page: number, limit: number): Observable<IProduct> {
-    return this.http.get<IProduct>(`${this.API}/products`, {
-      params: {
-        _per_page: limit,
-        _page: page,
-      },
-    });
+    return this.http
+      .get<IProduct>(`${this.API_URL}/products`, {
+        params: {
+          _per_page: limit,
+          _page: page,
+        },
+      })
+      .pipe(take(1));
+  }
+
+  post(product: IProductItem): Observable<IProductItem> {
+    return this.http
+      .post<IProductItem>(`${this.API_URL}/products`, product)
+      .pipe(take(1));
   }
 }
